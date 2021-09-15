172109c97d7f70d5c2fb2c714827013f9d194eb3
Luke McRedmonde <lmcredmond@kefron.com>
2021-09-15 21:56:45 +0100

    commit changes as need to test on oculus

--- start/lecture2_5/app.js
+++ start/lecture2_5/app.js
@@ -26,7 +26,9 @@ class App{
 		
         const geometry = new THREE.TorusKnotBufferGeometry( 0.8, 0.3, 120, 16 ); 
         
-        const material = new THREE.MeshBasicMaterial( { color: 0xFF0000 });
+        //const material = new THREE.MeshBasicMaterial( { color: 0xFF0000 }); -- no light
+        //const material = new THREE.MeshLambertMaterial( { color: 0xFFFF00 }); -- has light
+        const material = new THREE.MeshPhongMaterial( { color: 0xFFFF00, specular:0x4444,shininess:60 });//shiny
 
         this.mesh = new THREE.Mesh( geometry, material );
         
